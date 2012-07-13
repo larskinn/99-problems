0e3b7fe19e91c4d430855df438e9d10863b461b5
Lars Kinn Ekroll <larskinn@gmail.com>
2012-07-13 11:14:56 +0200

    Problem 22

--- problems_21-28.hs
+++ problems_21-28.hs
@@ -4,3 +4,8 @@ insertAt :: a -> [a] -> Int -> [a]
 insertAt x [] _ = [x]
 insertAt x all@(y:ys) n | n <= 1 = x : all
                         | n  > 1 = y : insertAt x ys (n-1)
+
+-- Problem 22:
+-- Create a list containing all integers within a given range.
+range :: Integral n => n -> n -> [n]
+range m n = [m..n]
