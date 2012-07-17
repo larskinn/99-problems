6f2c59fc8551e26971e9e87075903886821c3be4
Lars Kinn Ekroll <larskinn@gmail.com>
2012-07-17 21:04:40 +0200

    Problem 23

--- problems_21-28.hs
+++ problems_21-28.hs
@@ -1,3 +1,6 @@
+import System.Random
+import Control.Monad (replicateM)
+
 -- Problem 21:
 -- Insert an element at a given position into a list.
 insertAt :: a -> [a] -> Int -> [a]
@@ -9,3 +12,14 @@ insertAt x all@(y:ys) n | n <= 1 = x : all
 -- Create a list containing all integers within a given range.
 range :: Integral n => n -> n -> [n]
 range m n = [m..n]
+
+-- Problem 23:
+-- Extract a given number of randomly selected elements from a list.
+-- (I'm assuming the extracted elements don't need to be distinct)
+rndSelect :: [a] -> Int -> IO [a]
+rndSelect [] _ = return []
+rndSelect xs n = replicateM n (get1 xs)
+                 where get1 xs = do gen <- newStdGen
+                                    let (idx, _) = randomR (0, length xs - 1) gen
+                                    return (xs !! idx)
+
